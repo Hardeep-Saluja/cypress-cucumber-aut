45cd28c7faad96bd4949c978d73b7b563d363b5c
HARDEEP SINGH SALUJA <HARDEEP_SALUJA@OUTLOOK.COM>
2024-10-18 20:22:25 +0530

    config changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,25 @@
 const {defineConfig} = require("cypress");
+const createBundler = require("@bahmutov/cyress-esbuild-preprocessor");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 async function setupNodeEvents(on, config) { 
   await preprocessor.addCucumberPreprocessorPlugin(on, config);
-  // webpack config goes here if required
+  
+  on(
+    "file:preprocessor",
+    createBundler({
+      plugins: [createEsbuildPlugin.default(config)],
+    })
+  );
   return config;
 }
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://ecommerce-playground.lambdatest.io",
     specPattern: "**/*.feature",
-    supportFile: false,
+    supportFile: "cypress/support/e2e.js",
     setupNodeEvents,
   },
 });
